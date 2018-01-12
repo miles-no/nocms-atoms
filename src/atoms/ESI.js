2d36ced2dfaff21b6f337d2e92a8c2869069f86a
mskutle <magne.skutle@gmail.com>
2018-01-12 10:40:26 +0100

    feat: ESI component

---
+++ src/atoms/ESI.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ESI extends Component {
+  render() {
+    const esi = String.fromCharCode(60) + 'esi:';
+    const include = 'include';
+
+    let output;
+    if (!!this.props.content) {
+      if (this.props.content.indexOf(esi) === 0 && global.environment === 'server') {
+        output = this.props.content.replace('jsonEncode=true', '');
+      } else {
+        output = this.props.content;
+      }
+    } else {
+      output = `${esi}${include} src="${this.props.src}" />`;
+    }
+
+    return (
+      <div dangerouslySetInnerHTML={{ __html: output }} />
+    );
+  }
+}
+
+ESI.propTypes = {
+  content: PropTypes.string,
+  src: PropTypes.string,
+};
