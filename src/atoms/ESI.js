e742cb9ac9e1a4d29fe636ac7b83e700b8dbcf83
iorg1 <jorgen.landsnes@miles.no>
2018-01-17 09:49:09 +0100

    feat: Updated ESI component to fetch data client side with ajax

--- src/atoms/ESI.js
+++ src/atoms/ESI.js
@@ -1,30 +1,49 @@
-/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ajax from 'nocms-ajax';
 
 export default class ESI extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      content: null,
+    };
+    if (global.environment !== 'server') {
+      this.loadSrc = this.loadSrc.bind(this);
+      this.loadSrc(props.src);
+    }
+  }
+  componentWillReceiveProps(props) {
+    if (props.src !== this.props.src) {
+      this.loadSrc(props.src);
+    }
+  }
+  loadSrc(src) {
+    if (global.environment !== 'server') {
+      ajax.get(src, { contentType: 'text/html' }, (err, res) => {
+        if (err) {
+          return;
+        }
+        this.setState({ content: res });
+      });
+    }
+  }
   render() {
-    const esi = String.fromCharCode(60) + 'esi:';
+    const esi = `${String.fromCharCode(60)}esi:`;
     const include = 'include';
-
-    let output;
-    if (!this.props.content) {
-      if (this.props.content.indexOf(esi) === 0 && global.environment === 'server') {
-        output = this.props.content.replace('jsonEncode=true', '');
-      } else {
-        output = this.props.content;
-      }
-    } else {
+    let output = '';
+    if (global.environment === 'server') {
       output = `${esi}${include} src="${this.props.src}" />`;
+    } else {
+      output = this.state.content || '';
     }
 
     return (
-      <div dangerouslySetInnerHTML={{ __html: output }} />
+      <div dangerouslySetInnerHTML={{ __html: output }} /> // eslint-disable-line
     );
   }
 }
 
 ESI.propTypes = {
-  content: PropTypes.string,
   src: PropTypes.string,
 };
